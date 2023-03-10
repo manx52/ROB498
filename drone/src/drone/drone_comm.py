7a00f89fa3ef21c8ab43930beebd521718272b05
manx52 <jonathanspraggett@gmail.com>
2023-03-10 01:38:11 -0500

    Finished waypoint navigation and added vicon

--- drone/src/drone/drone_comm.py
+++ drone/src/drone/drone_comm.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import math
 import rospy
-from geometry_msgs.msg import Pose, PoseStamped, PoseWithCovarianceStamped, TransformStamped
+from geometry_msgs.msg import Pose, PoseStamped, PoseArray, TransformStamped, Point, Quaternion
 from mavros_msgs.msg import State
 from mavros_msgs.srv import CommandBool, CommandBoolRequest, SetMode, SetModeRequest
 from std_srvs.srv import Empty, EmptyResponse
 from nav_msgs.msg import Odometry
+
 
 class DroneComm:
     def __init__(self):
@@ -16,6 +17,10 @@ class DroneComm:
 
         node_name = 'rob498_drone_07'
         rospy.init_node(node_name)
+
+        # Settings
+        self.offset = 0.1
+        self.launch_height = 1.5
 
         # Simulation
         self.sim = rospy.get_param("/simulation")
@@ -30,15 +35,22 @@ class DroneComm:
         self.bool_land = False
         self.bool_abort = False
         self.vicon_enabled = False
+        self.challenge2 = False
 
         # Var
         self.drone_onboard_pose = PoseStamped()
-        self.drone_vicon_pose = TransformStamped()
+        self.drone_vicon_pose = PoseStamped()
 
         self.waypoint_goal = PoseStamped()
+        self.waypoint_goal.header.stamp = rospy.Time.now()
+        # self.waypoint_goal.header.frame_id = "world"  # TODO
         self.waypoint_goal.pose.position.x = 0
         self.waypoint_goal.pose.position.y = 0
         self.waypoint_goal.pose.position.z = 0
+        self.waypoints = PoseArray()
+        self.waypoints.header.stamp = rospy.Time.now()
+        # self.waypoints.header.frame_id = "world"  # TODO
+        self.waypoint_index = 0
 
         self.current_state = State()
         self.r = rospy.Rate(30)
@@ -62,6 +74,7 @@ class DroneComm:
 
         # Publishers
         self.local_pos_pub = rospy.Publisher("mavros/setpoint_position/local", PoseStamped, queue_size=10)
+        self.vicon_pose_pub = rospy.Publisher("mavros/vision_pose/pose", PoseStamped, queue_size=10)
 
     # Callback Sensors
     def pose_callback(self, msg):
@@ -72,7 +85,12 @@ class DroneComm:
 
     def vicon_callback(self, msg):
         if self.vicon_enabled:
-            self.drone_vicon_pose = msg
+            self.drone_vicon_pose.header = msg.header
+            self.drone_vicon_pose.pose.position.x = msg.transform.translation.x
+            self.drone_vicon_pose.pose.position.y = msg.transform.translation.y
+            self.drone_vicon_pose.pose.position.z = msg.transform.translation.z
+            self.drone_vicon_pose.pose.orientation = msg.transform.rotation
+            self.vicon_pose_pub.publish(self.drone_vicon_pose)
 
     # Callback handlers
 
@@ -83,9 +101,10 @@ class DroneComm:
         self.bool_land = False
         self.bool_abort = False
 
+        self.waypoint_goal.header.stamp = rospy.Time.now()
         self.waypoint_goal.pose.position.x = 0
         self.waypoint_goal.pose.position.y = 0
-        self.waypoint_goal.pose.position.z = 1.5
+        self.waypoint_goal.pose.position.z = self.launch_height - self.offset
 
     def handle_test(self):
         print('Test Requested. Your drone should perform the required tasks. Recording starts now.')
@@ -94,9 +113,48 @@ class DroneComm:
         self.bool_land = False
         self.bool_abort = False
 
-        self.waypoint_goal.pose.position.x = 0
-        self.waypoint_goal.pose.position.y = 0
-        self.waypoint_goal.pose.position.z = 1.5
+        if self.challenge2:
+            self.waypoint_goal.header.stamp = rospy.Time.now()
+            self.waypoint_goal.pose.position.x = 0
+            self.waypoint_goal.pose.position.y = 0
+            self.waypoint_goal.pose.position.z = self.launch_height - self.offset
+        else:
+            self.waypoint_index = 0
+
+            # 4 pt square
+            pt1 = Point(1, 0, self.launch_height - self.offset)
+            pt2 = Point(1, 1, self.launch_height - self.offset)
+            pt3 = Point(0, 1, self.launch_height - self.offset)
+            pt4 = Point(0, 0, self.launch_height - self.offset)
+            q1 = Quaternion(0, 0, 0, 1)
+            self.waypoints.poses = [
+                Pose(pt1, q1),
+                Pose(pt2, q1),
+                Pose(pt3, q1),
+                Pose(pt4, q1)
+            ]
+            # 8 pt square
+            # pt1 = Point(1, 0, self.launch_height - self.offset)
+            # pt2 = Point(1, 1, self.launch_height - self.offset)
+            # pt3 = Point(1, 2, self.launch_height - self.offset)
+            # pt4 = Point(0, 2, self.launch_height - self.offset)
+            # pt5 = Point(-1, 2, self.launch_height - self.offset)
+            # pt6 = Point(-1, 1, self.launch_height - self.offset)
+            # pt7 = Point(-1, 0, self.launch_height - self.offset)
+            # pt8 = Point(0, 0, self.launch_height - self.offset)
+            #
+            # q1 = Quaternion(0, 0, 0, 1)
+            # self.waypoints.poses = [
+            #     Pose(pt1, q1),
+            #     Pose(pt2, q1),
+            #     Pose(pt3, q1),
+            #     Pose(pt4, q1),
+            #     Pose(pt5, q1),
+            #     Pose(pt6, q1),
+            #     Pose(pt7, q1),
+            #     Pose(pt8, q1),
+            # ]
+
 
     def handle_land(self):
         print('Land Requested. Your drone should land.')
@@ -105,9 +163,10 @@ class DroneComm:
         self.bool_land = True
         self.bool_abort = False
 
+        self.waypoint_goal.header.stamp = rospy.Time.now()
         self.waypoint_goal.pose.position.x = self.drone_onboard_pose.pose.position.x
         self.waypoint_goal.pose.position.y = self.drone_onboard_pose.pose.position.y
-        self.waypoint_goal.pose.position.z = -0.3
+        self.waypoint_goal.pose.position.z = -0.3 - self.offset
 
     def handle_abort(self):
         print('Abort Requested. Your drone should land immediately due to safety considerations')
@@ -116,9 +175,10 @@ class DroneComm:
         self.bool_land = False
         self.bool_abort = True
 
+        self.waypoint_goal.header.stamp = rospy.Time.now()
         self.waypoint_goal.pose.position.x = self.drone_onboard_pose.pose.position.x
         self.waypoint_goal.pose.position.y = self.drone_onboard_pose.pose.position.y
-        self.waypoint_goal.pose.position.z = -0.3
+        self.waypoint_goal.pose.position.z = -0.3 - self.offset
 
     # Service callbacks
     def callback_launch(self, request):
@@ -172,8 +232,6 @@ class DroneComm:
         last_req = rospy.Time.now()
 
         while not rospy.is_shutdown():
-            # Update waypoint
-            pose = self.waypoint_goal
 
             if self.current_state.mode != "OFFBOARD" and (rospy.Time.now() - last_req) > rospy.Duration(5.0):
                 if self.set_mode_client.call(offb_set_mode).mode_sent:
@@ -186,10 +244,24 @@ class DroneComm:
                         rospy.loginfo("Vehicle armed")
 
                     last_req = rospy.Time.now()
+            # Update waypoint
+            if self.bool_test:
+                # Are we there yet?
+                pose = PoseStamped()
+                pose.header.stamp = rospy.Time.now()
+                pose.pose = self.waypoints.poses[self.waypoint_index]
+                error_pose = self.waypoint_error(pose)
+
+                if error_pose < 0.1 and self.waypoint_index < (len(self.waypoints.poses)-1):  # TODO tune
+                    self.waypoint_index += 1
+                    pose.header.stamp = rospy.Time.now()
+                    pose.pose = self.waypoints.poses[self.waypoint_index]
+            else:
+                pose = self.waypoint_goal
 
             error_pose = self.waypoint_error(pose)
-            msg = "Waypoint: %s" % error_pose
-            rospy.loginfo_throttle(1, msg)
+            msg = "Waypoint {index:} error: {error:}"
+            rospy.loginfo_throttle(1, msg.format(index=self.waypoint_index, error=error_pose))
 
             # Send goal waypoint
             self.local_pos_pub.publish(pose)
